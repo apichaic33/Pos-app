ba981306fe74c811f9b4f1221017eef8e2661e8d
apichaic33 <q.apichai@gmail.com>
2026-03-08 03:59:22 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// POS-APP Service Worker
+const CACHE_NAME = 'pos-app-v1';
+const ASSETS = ['./'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      }))
+      .catch(() => caches.match('./'))
+  );
+});
